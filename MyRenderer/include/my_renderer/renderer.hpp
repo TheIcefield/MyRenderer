ec9f7882f95aade504f68886aa1c462242a18875
TheIcefield <oleg.icefield@gmail.com>
2026-05-12 20:22:53 +0700

    add renderer class #0

---
+++ MyRenderer/include/my_renderer/renderer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <expected>
+#include <string_view>
+
+namespace my_renderer {
+
+class Renderer {
+public:
+    void draw() const;
+
+    static std::expected<Renderer, std::string_view> create(void *proc_addr);
+};
+
+} // namespace my_renderer
---
+++ MyRenderer/src/my_renderer/gl/renderer.cpp
@@ -0,0 +1,28 @@
+#include <my_renderer/renderer.hpp>
+#include "../logger.hpp"
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+
+#include <expected>
+
+namespace my_renderer {
+
+std::expected<Renderer, std::string_view> Renderer::create(void *proc_addr) {
+    if (!gladLoadGL(reinterpret_cast<GLADloadfunc>(proc_addr))) {
+        LOG_CRITICAL("gladLoadGL() failed!");
+        return std::unexpected("gladLoadGL() failed!");
+    }
+
+    LOG_INFO("Renderer initialized. GLADloadfunc: {0}", proc_addr);
+
+    glClearColor(0, 0, 0, 1);
+
+    return Renderer{};
+}
+
+void Renderer::draw() const {
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+} // namespace my_renderer
